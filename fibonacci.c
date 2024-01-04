2568635c5d7f1d8f01b89c6c32da5e04725902c4
ANSHUMAN RAJ SINGH <146862435+Anshuman-Raj-Singh@users.noreply.github.com>
2024-01-04 20:20:41 +0530

    fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int fibonacci(int n) {
+    if (n <= 1)
+        return n;
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
+}
+void printFibonacciSeries(int terms) {
+    printf("Fibonacci Series up to %d terms:\n", terms);
+    for (int i = 0; i < terms; ++i) {
+        printf("%d ", fibonacci(i));
+    }
+    printf("\n");
+}
+int main() {
+    int terms;
+    printf("Enter the number of terms for Fibonacci series: ");
+    scanf("%d", &terms);
+    printFibonacciSeries(terms);
+    return 0;
+}
